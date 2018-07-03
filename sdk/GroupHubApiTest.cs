02aa9b16879874b19b29122c6021c903e3b3e944
Baron Zhang <baron.zhang@avepoint.com>
2018-07-03 16:07:18 +0800

    add group hub sample code project

---
+++ sdk/GroupHubApiTest.cs
@@ -0,0 +1,63 @@
+﻿namespace Cloud.GroupHub.Samples.Sdk
+{
+    #region using directives
+
+    using System;
+    using GroupHub.Sdk;
+    using Xunit;
+    using Xunit.Abstractions;
+
+    #endregion using directives
+
+    /// <summary>
+    /// GaoApiTest class shows the initial process of the GaoApi sdk, please refer to the
+    /// CombineGaoApi test case for detail process. You should replace the datacenter, username and
+    /// password to your own to use the API
+    /// </summary>
+    public class GaoApiTest : IClassFixture<GroupHubApiClassFixtrue>
+    {
+        public GaoApiTest(ITestOutputHelper testOutput, GroupHubApiClassFixtrue fixtrue)
+        {
+            this.fixtrue = fixtrue;
+            this.output = testOutput;
+        }
+
+        private readonly GroupHubApiClassFixtrue fixtrue;
+        private readonly ITestOutputHelper output;
+
+        [Trait("Owner", "Baron")]
+        [Theory(DisplayName = "GaoApi.Init 001")]
+        [InlineData(Region.SoutheastAsia, "baron@baron.sapce", "baron@baron.space")]
+        public void Init001(Region region, String username, String password)
+        {
+            Assert.Equal(1, this.fixtrue.ExecutionCount);
+            Assert.True(this.fixtrue.InitStatus);
+            this.output.WriteLine("GaoApi class should be initialized once");
+        }
+
+        [Trait("Owner", "Baron")]
+        [Theory(DisplayName = "GaoApi")]
+        [InlineData(Region.SoutheastAsia, "baron@baron.sapce", "baron@baron.space")]
+        public void CombineGaoApi(Region region, String username, String password)
+        {
+        }
+
+        [Trait("Owner", "Baron")]
+        [Fact(DisplayName = "GaoApi.Create")]
+        public void Create()
+        {
+        }
+
+        [Trait("Owner", "Baron")]
+        [Fact(DisplayName = "GaoApi.Init")]
+        public void Init()
+        {
+            //The code style here is leverage the xUnit test style, you should gurantee that
+            //your GaoApi.Init method run once before you can invoke the other GaoApi interface.
+            //you can find the Init sample in the GaoApiClassFixture.cs
+            Assert.Equal(1, this.fixtrue.ExecutionCount);
+            Assert.True(this.fixtrue.InitStatus);
+            this.output.WriteLine("GaoApi class should be initialized once");
+        }
+    }
+}
---
+++ sdk/GroupHubClassFixtrue.cs
@@ -0,0 +1,24 @@
+﻿namespace Cloud.GroupHub.Samples.Sdk
+{
+    #region using directives
+
+    using System;
+
+    #endregion using directives
+
+    /// <summary>
+    /// GaoApiClassFixtrue is a global initial class for GaoApi client context initializaion.
+    /// </summary>
+    public class GroupHubApiClassFixtrue
+    {
+        public GroupHubApiClassFixtrue()
+        {
+            this.InitStatus = true;
+
+            this.ExecutionCount++;
+        }
+
+        public Boolean InitStatus { get; }
+        public Int32 ExecutionCount { get; }
+    }
+}
